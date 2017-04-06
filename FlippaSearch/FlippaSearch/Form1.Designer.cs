681fb6377dff07f9058b39f65aef3207557a1fbd
Justin-PC <justinkowalsky@live.com>
2017-04-05 23:17:17 -0400

    Added progress bar

--- FlippaSearch/FlippaSearch/Form1.Designer.cs
+++ FlippaSearch/FlippaSearch/Form1.Designer.cs
@@ -33,11 +33,14 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.monthlyUsers = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(93, 207);
+            this.button1.Location = new System.Drawing.Point(12, 207);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -56,9 +59,11 @@
             // 
             this.textBox1.BackColor = System.Drawing.SystemColors.Control;
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Enabled = false;
             this.textBox1.Location = new System.Drawing.Point(12, 127);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
             this.textBox1.Size = new System.Drawing.Size(104, 30);
             this.textBox1.TabIndex = 1;
             this.textBox1.Text = "Please input your Organic Search %:";
@@ -74,23 +79,55 @@
             // 
             this.textBox3.BackColor = System.Drawing.SystemColors.Control;
             this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Enabled = false;
             this.textBox3.Location = new System.Drawing.Point(12, 64);
             this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
             this.textBox3.Size = new System.Drawing.Size(100, 46);
             this.textBox3.TabIndex = 3;
             this.textBox3.Text = "Please enter your minimum number of monthly users.";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.ForeColor = System.Drawing.Color.LawnGreen;
+            this.progressBar1.Location = new System.Drawing.Point(12, 12);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(211, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(130, 207);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Stop!";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(235, 271);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.monthlyUsers);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.searchPercent);
             this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -105,6 +142,9 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox monthlyUsers;
         private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- FlippaSearch/FlippaSearch/Form1.cs
+++ FlippaSearch/FlippaSearch/Form1.cs
@@ -1,18 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Support;
 using OpenQA.Selenium.Chrome;
 using System.Text.RegularExpressions;
 using System.IO;
 using OpenQA.Selenium.Support.UI;
+using System.Threading;
 
 namespace FlippaSearch
 {
@@ -22,21 +18,27 @@ namespace FlippaSearch
         public Form1()
         {
             driverGC = new ChromeDriver(@"Z:\Justin\Documents\Visual Studio 2015\chromedriver_win32");
-            
             InitializeComponent();
-            
         }
-
         private void button1_Click(object sender, EventArgs e)
         {
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            File.WriteAllText(@"C:\Users\Justin\Desktop\newFile.txt", string.Empty);
+            int myNewProgress = 0;
             int monthlyUserText = Convert.ToInt32(monthlyUsers.Text);
             driverGC.Navigate().GoToUrl("https://flippa.com/websites/starter-sites?sitetype=blog&uniques_per_month_min=" + monthlyUserText);
             List<IWebElement> starterSites = new List<IWebElement>();
-            List<String> myStarterSites = new List<string>();;
+            List<String> myStarterSites = new List<string>(); ;
             List<String> mySiteSearch = new List<string>();
-            var numPages = (driverGC.FindElement(By.XPath("//*[@id='searchBody']/div[1]/div[1]/h2/span")).Text);
+            string numPages = (driverGC.FindElement(By.XPath("//*[@id='searchBody']/div[1]/div[1]/h2/span")).Text);
             double numberPages = int.Parse(Regex.Match(numPages, @"\d+", RegexOptions.RightToLeft).Value);
-            numberPages = Math.Ceiling(numberPages / 50);
+            double mynumberPages = Math.Ceiling(numberPages / 50);
+            int myNumPages = Convert.ToInt32(numberPages);
+            MessageBox.Show(numberPages.ToString());
             int j;
             for (int i = 1; i <= numberPages; i++)
             {
@@ -52,7 +54,7 @@ namespace FlippaSearch
                         siteLink = mySites[j].FindElement(By.CssSelector(".ListingResults___listingResultLink")).GetAttribute("href");
                         driverGC.Navigate().GoToUrl(siteLink);
                     }
-                    catch(System.ArgumentOutOfRangeException)
+                    catch (System.ArgumentOutOfRangeException)
                     {
                         continue;
                     }
@@ -73,6 +75,7 @@ namespace FlippaSearch
                                 CellValue = driverGC.FindElement(By.XPath("/html/body/div[3]/div[1]/div[1]/div[5]/div[1]/div/table[1]/tbody/tr[" + k + "]/td[" + b + "]")).Text.ToString();
                                 if (CellValue == "Organic Search")
                                 {
+                                    //MessageBox.Show("Found organic search");
                                     String mySiteName = driverGC.FindElement(By.XPath("/html/body/div[3]/div[1]/div[1]/div[1]/div[1]/h1")).Text.ToString();
                                     newCellValue = driverGC.FindElement(By.XPath("/html/body/div[3]/div[1]/div[1]/div[5]/div[1]/div/table[1]/tbody/tr[" + k + "]/td[3]")).Text.ToString();
                                     cellValueChange = Regex.Replace(newCellValue, @"[%\s]", string.Empty);
@@ -90,35 +93,35 @@ namespace FlippaSearch
                                         {
                                             foreach (string s in myStarterSites)
                                             {
-                                                writer.WriteLine(s); 
+                                                writer.WriteLine(s);
                                             }
                                         }
                                     }
-                                       
+
                                 }
                             }
                         }
                     }
                     catch (NoSuchElementException)
                     {
+                        driverGC.Navigate().Back();
                         continue;
                     }
-                    //testing tables
+                    int myProgress = (100 / (int)numberPages);
+                    myNewProgress = myNewProgress + myProgress;
+                    Thread.Sleep(300);
+                    backgroundWorker1.ReportProgress(myNewProgress, "Working...");
                     driverGC.Navigate().Back();
-                    driverGC.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
-                    //write shit to file
                     siteLink = null;
                 }
                 mySites = null;
                 try
                 {
                     driverGC.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
-                    //driverGC.FindElement(By.XPath("//*[@id='searchBody']/div[2]/div[2]/div/a[3]")).Click();
                     driverGC.FindElement(By.XPath("//*[@id='searchBody']/div[2]/div[2]/div/a[3]")).Click(); // go to next page
                     WebDriverWait wait = new WebDriverWait(driverGC, TimeSpan.FromSeconds(5));
                     wait.Until(ExpectedConditions.TextToBePresentInElementLocated(By.CssSelector(".Pagination___pageLink  Pagination___prevLink"), (i + 1).ToString()));
-                    //driverGC.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(30);
-                    
+
                 }
                 catch (NoSuchElementException)
                 {
@@ -130,14 +133,31 @@ namespace FlippaSearch
                 }
                 catch (ElementNotVisibleException)
                 {
-                    Console.WriteLine("No more pages");
+                    continue;
                 }
-               
             }
-            
             //MessageBox.Show("End");
             driverGC.Quit();
-            Application.Exit();    
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {          
+            driverGC.Quit();
+            Application.Exit();
+            //backgroundWorker1.CancelAsync();
+        }
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {              
+                MessageBox.Show("You've cancelled the backgroundworker!");
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
         }
     }
 }
